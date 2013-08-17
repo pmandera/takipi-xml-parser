775371330267dc2ad2cf79ba8fb8257a7646f0f1
Paweł Mandera <pawel.mandera@ugent.be>
2013-08-17 16:32:13 +0200

    Working agglutinative attachment
    Added recursive function to deal with agglutinative 'być'.
    Takipi splits verbs like 'wszedłem' into two tokens: 'wszedł', 'em'
    This parser puts them back together.

--- Parse.scala
+++ Parse.scala
@@ -1,16 +1,15 @@
-case class Tag(lemma: String, pos: String)
+case class Tag(lemma: String, pos: String) {
+  lazy val splPos = pos.split(':')
+  lazy val aglt = splPos(0) == "aglt"
+}
 
 case class TaggedToken(spelling: String, tag: Tag) {
   def tabString = spelling + "\t" + tag.pos + "\t" + tag.lemma
+  lazy val aglt = tag.aglt
 }
 
-object Parse extends App {
-  val sourceDir = "../tmp_tagged/"
-  val targDir = "../tagged/"
-  val files = new java.io.File(sourceDir).listFiles map (_.getName)
 
-  files.par foreach { e => save(targDir + e, parse(sourceDir + e)) }
-
+object Parser {
   def parse(fname: String) = {
     println("Parsing " + fname)
 
@@ -21,10 +20,32 @@ object Parse extends App {
     val disamb = disambre.findAllIn(data).matchData map { e => Tag(e.group(1), e.group(2)) }
     orth zip disamb map { e => e match {
       case (a, b) => TaggedToken(a, b)
-    } }
+    } } toList
   }
 
-  def save(fname: String, tagged: Iterator[TaggedToken]) = {
+  def agltFilter(t: List[TaggedToken]): List[TaggedToken] = {
+    // optimized for tail recursion
+    @scala.annotation.tailrec
+    def iter(t: List[TaggedToken], acc: List[TaggedToken]): List[TaggedToken] = t match {
+      case Nil => acc
+      case x :: xs if x.aglt => iter(xs.tail, TaggedToken(xs.head.spelling + x.spelling, xs.head.tag) :: acc)
+      case x :: xs => iter(xs, x :: acc)
+    }
+    iter(t, Nil)
+  }
+
+  def filterByc(tagged: List[TaggedToken]): List[TaggedToken] = agltFilter(tagged.reverse) 
+}
+
+object Parse extends App {
+  val sourceDir = "../toy/tagged/"
+  val targDir = "../toy/tagged/parsed/"
+  val files = new java.io.File(sourceDir).listFiles map (_.getName)
+
+  files.par foreach { e => save(targDir + e, Parser.filterByc(Parser.parse(sourceDir + e))) }
+  // val a = files map { e => Parser.filterByc(Parser.parse(sourceDir + e)) } foreach { println _ }
+
+  def save(fname: String, tagged: Seq[TaggedToken]) = {
      val p = new java.io.PrintWriter(fname)
      tagged foreach { e => p.println(e.tabString) }
      p.close()
